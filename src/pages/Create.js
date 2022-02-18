6d34482f05c87f50b044db1dcdb9bdaf0836ae0e
Inzamam Baig <inzamam.baig@broadvisions.net>
2022-02-18 10:50:32 +0500

    Material tutorial netninja

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,71 @@
-import React from 'react'
+import SendIcon from "@material-ui/icons/Send";
+import { Container, Button, Typography, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 20,
+    backgroundColor: "orange",
+    "&:hover": {
+      backgroundColor: "#000",
+    },
+  },
+});
 
 export default function Create() {
+  const classes = useStyles();
   return (
-    <div>
-      Create page
-    </div>
-  )
+    <Container>
+      {/* <Typography variant="h1" color="primary" align="center">
+        Create a new note
+      </Typography> */}
+      {/* <Typography variant="h1" color="secondary" align="center">
+        Create a new note
+      </Typography>
+      <Typography noWrap>
+        lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor
+        sit amet lorem ipsum dolor sit amet lorem ips lorem ipsum dolor sit amet
+        lorem lorem ipsum lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+        lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ips lorem
+        ipsum dolor sit amet lorem lorem ipsum lorem ipsum dolor sit amet lorem
+        ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit
+        amet lorem ips lorem ipsum dolor sit amet lorem lorem ipsum
+      </Typography>
+      <Typography variant="caption">
+        Lorem ipsum dolor sit amet lorem ipsum d
+      </Typography> */}
+
+      <Typography
+        variant="h6"
+        component="h2"
+        gutterBottom
+        color="textSecondary"
+      >
+        Create a new note
+      </Typography>
+
+      {/* <Button type="submit" color="secondary" variant="outlined">
+        Create
+      </Button>
+
+      <ButtonGroup color="primary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+
+      <Button
+        type="submit"
+        color="secondary"
+        variant="contained"
+        disableElevation
+        className={classes.btn}
+        endIcon={<SendIcon fontSize="small" />}
+        onClick={(e) => console.log(e.target)}
+      >
+        Create
+      </Button>
+      <br />
+      {/* <AdbIcon color="primary" fontSize="large" /> */}
+    </Container>
+  );
 }
